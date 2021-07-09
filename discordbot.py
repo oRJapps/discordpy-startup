9c286e53bb3ce65990b44c697fc112d6dc0ac8e8
XRJX <nasulrich@gmail.com>
2021-07-09 20:08:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,45 @@
-from discord.ext import commands
+import discord
+from discord import message
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client = discord.Client()
 
+# チャンネル入退室時の通知処理
+@client.event
+async def on_voice_state_update(member, before, after):
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+    if before.channel != after.channel:
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(863009355115528192)
 
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        #announceChannelIds = client.channel.name
 
-bot.run(token)
+        # 退室通知
+        if before.channel is None:
+             await botRoom.send("**" + after.channel.name + "```** に、__" + member.name + "__  が参加しました！```")
+        elif after.channel is None:
+             await botRoom.send("**" + before.channel.name + "```** から、__" + member.name + "__  が抜けました！```")
+        #if before.channel is not None and before.channel.id in announceChannelIds:
+        #    await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けました！")
+
+        #if after.channel is not None and after.channel.id in announceChannelIds:
+        #    await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しました！")
+
+#メッセージ監視Bot
+@client.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+
+    CHANNEL_ID = 862362251399004190
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(f"{message.author.name}の削除メッセージ```{message.content}```")
+    
+client.run(token)

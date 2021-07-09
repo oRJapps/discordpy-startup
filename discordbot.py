e197545c2bc68201a9074c90719a8019bd38bc24
XRJX <nasulrich@gmail.com>
2021-07-10 01:01:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_voice_state_update(member, before, after):
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
     if before.channel != after.channel:
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(863009355115528192)
+        botRoom = client.get_channel(863011085246529576)
 
         # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
         #announceChannelIds = client.channel.name

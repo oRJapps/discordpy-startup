c6ad1cf5f73f42a85ea0ceb0ba7f8fa523aec577
XRJX <nasulrich@gmail.com>
2021-07-11 11:05:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,54 @@
-from discord.ext import commands
+import discord
+from discord import message
+from discord import Embed
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
+# チャンネル入退室時の通知処理
+@client.event
+async def on_voice_state_update(member, before, after):
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+    if before.channel != after.channel:
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(863009355115528192)
 
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        #announceChannelIds = client.channel.name
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+        # 入退室通知
+        if before.channel is None:
+             #await botRoom.send("**" + after.channel.name + "```** に、__" + member.name + "__  が参加しました！```")
+             if after.channel.id != 856777019530412103:
+                 embed=discord.Embed(title="[VC:"+after.channel.name+"]" + member.name +"が参加しました！",description="一般ボイスチャンネル",color=discord.Colour.green())
+                 await botRoom.send(embed=embed)
+                 Invite = await after.channel.create_invite()
+                 await botRoom.send(Invite.url)
+             elif after.channel.id == 856777019530412103:
+                embed=discord.Embed(title="[VC:"+after.channel.name+"]" + "誰かが参加しました！",description="Not一般ボイスチャンネル",color=discord.Colour.pink())
+                embed.add_field(name="⚠注意",value="トーク内容にエロ・グロ系が含まれる可能性があるので、参加は中学生以降自己責任です")
+                await botRoom.send(embed=embed)
 
+        elif after.channel is None:
+             await botRoom.send("```**" + before.channel.name + "** から、__" + member.name + "__  が抜けました！```")
+        #if before.channel is not None and before.channel.id in announceChannelIds:
+        #    await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けました！")
 
-bot.run(token)
+        #if after.channel is not None and after.channel.id in announceChannelIds:
+        #    await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しました！")
+
+#メッセージ監視Bot
+@client.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+
+    CHANNEL_ID = 863266385965744180
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(f"{message.author.name}の削除メッセージ```{message.content}```")
+
+# Botのトークンを指定（デベロッパーサイトで確認可能）
+client.run(token)

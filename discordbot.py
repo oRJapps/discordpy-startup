c325b3c1472e3dbc5724997951c93e2edfa09c15
XRJX <nasulrich@gmail.com>
2021-07-17 12:00:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,8 @@ import discord
 from discord import message
 from discord import Embed
 from discord.ext import commands
+import os
+import traceback
 import logging
 
 token = os.environ['DISCORD_BOT_TOKEN']

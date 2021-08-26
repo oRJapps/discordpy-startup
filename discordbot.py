267bf49ea66dec5412dace287bb70724fc626c86
XRJX <nasulrich@gmail.com>
2021-08-26 20:34:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,8 @@ async def on_voice_state_update(member, before, after):
         if before.channel is None:
              #await botRoom.send("**" + after.channel.name + "```** に、__" + member.name + "__  が参加しました！```")
              if after.channel.id != 856777019530412103:
-                 embed=discord.Embed(title=member.name +"が参加しました！",description="参加チャンネル：["+after.channel.name+"]",color=discord.Colour.green())
                  Invite = await after.channel.create_invite()
+                 embed=discord.Embed(title=member.name +"が参加しました！",description="参加チャンネル：["+after.channel.name+"]",color=discord.Colour.green())                 
                  embed.set_footer(text=Invite.url)
                  embed.set_thumbnail(url=member.avatar_url)
                  await botRoom.send(embed=embed)
